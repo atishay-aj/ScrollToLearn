ab5a7e0e8e7c9fd75792befe11a8832793c93499
Atishay_Jain <atishayjain759@gmail.com>
2023-06-14 13:04:02 +0000

    updating design of post

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(LearntoScrollApp());
+  runApp(ScrollToLearnApp());
 }
 
-class LearntoScrollApp extends StatelessWidget {
+class ScrollToLearnApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Learn to Scroll',
+      title: 'Scroll To Learn',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomePage(),
     );
@@ -24,7 +25,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Learn to Scroll'),
+        title: Text('Scroll To Learn'),
       ),
       body: PostListView(),
     );
@@ -40,6 +41,7 @@ class _PostListViewState extends State<PostListView> {
   List<Post> posts = [];
   ScrollController _scrollController = ScrollController();
   int _page = 1;
+  bool _isLoading = false;
 
   @override
   void initState() {
@@ -61,25 +63,32 @@ class _PostListViewState extends State<PostListView> {
   }
 
   void fetchPosts() async {
-    final url =
-        'https://jsonplaceholder.typicode.com/photos?_page=$_page&_limit=10';
-    final response = await http.get(Uri.parse(url));
-
-    if (response.statusCode == 200) {
-      final List<dynamic> responseData = jsonDecode(response.body);
-      final List<Post> fetchedPosts = responseData.map((data) {
-        return Post(
-          imageUrl: data['url'],
-          text: data['title'],
-        );
-      }).toList();
-
+    if (!_isLoading) {
       setState(() {
-        posts.addAll(fetchedPosts);
-        _page++;
+        _isLoading = true;
       });
-    } else {
-      print('Error fetching posts. Status code: ${response.statusCode}');
+
+      final url =
+          'https://jsonplaceholder.typicode.com/photos?_page=$_page&_limit=10';
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final List<dynamic> responseData = jsonDecode(response.body);
+        final List<Post> fetchedPosts = responseData.map((data) {
+          return Post(
+            imageUrl: data['url'],
+            text: data['title'],
+          );
+        }).toList();
+
+        setState(() {
+          posts.addAll(fetchedPosts);
+          _page++;
+          _isLoading = false;
+        });
+      } else {
+        print('Error fetching posts. Status code: ${response.statusCode}');
+      }
     }
   }
 
@@ -87,9 +96,25 @@ class _PostListViewState extends State<PostListView> {
   Widget build(BuildContext context) {
     return ListView.builder(
       controller: _scrollController,
-      itemCount: posts.length,
+      itemCount: posts.length + 1,
       itemBuilder: (context, index) {
-        return PostWidget(post: posts[index]);
+        if (index < posts.length) {
+          return Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: PostWidget(post: posts[index]),
+          );
+        } else if (_isLoading) {
+          return Container(
+            padding: EdgeInsets.all(16.0),
+            alignment: Alignment.center,
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+            ),
+          );
+        } else {
+          return SizedBox();
+        }
       },
     );
   }
@@ -110,15 +135,48 @@ class PostWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      elevation: 4.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+      ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Image.network(
-            post.imageUrl,
-            fit: BoxFit.cover,
+          Container(
+            height: 200.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(8.0)),
+              image: DecorationImage(
+                  image: NetworkImage(post.imageUrl), fit: BoxFit.fill),
+            ),
           ),
           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(post.text),
+            padding: const EdgeInsets.all(12.0),
+            child: Text(
+              post.text,
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                icon: Icon(Icons.favorite_border),
+                onPressed: () {
+                  // Handle like button pressed
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.bookmark_border),
+                onPressed: () {
+                  // Handle save button pressed
+                },
+              ),
+            ],
           ),
         ],
       ),
